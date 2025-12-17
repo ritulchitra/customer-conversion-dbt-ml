1aea4277c9e14f4a0aea0e89bb69080e67869f36
Ritul Chitra Sharma <ritulchitrasharma3456@gmail.com>
2025-12-17 09:18:18 +0530

    1 st draft of project is done

---
+++ target/compiled/supabase_project/dbt/supabase_project/models/intermediate/int_customer_orders.sql
@@ -0,0 +1,9 @@
+select
+    customer_id,
+    count(distinct order_id) as total_orders,
+    sum(quantity * unit_price) as total_spend,
+    avg(quantity * unit_price) as avg_order_value,
+    max(order_timestamp) as last_order_timestamp
+from "postgres"."public"."stg_orders"
+where customer_id is not null
+group by customer_id
